89c49c9c56a605a950eea725ddbcfd825b58a017
jljjjljj <144417388+jljjjljj@users.noreply.github.com>
2023-12-05 15:14:56 +1100

    1
    1

--- genshin_music/src/components/Homerecommend.jsx
+++ genshin_music/src/components/Homerecommend.jsx
@@ -63,30 +63,22 @@ const Homerecommend = () => {
   const [tracks4, setTracks4] = useState([]);
   const [playlistDetail4, setPlaylistDetail4] = useState([]);
   const [tracks5, setTracks5] = useState([]);
+  const [playlistDetail5, setPlaylistDetail5] = useState([]);
+  const [tracks6, setTracks6] = useState([]);
 
+  const list=[
+    {imgUrl:"/img/re1.png",name:playlistDetail.name,trackState:tracks},
+    {imgUrl:"/img/re2.png",name:playlistDetail1.name,trackState:tracks2},
+    {imgUrl:"/img/re3.png",name:playlistDetail2.name,trackState:tracks3},
+    {imgUrl:"/img/re4.png",name:playlistDetail3.name,trackState:tracks4},
+    {imgUrl:"/img/re5.png",name:playlistDetail4.name,trackState:tracks5},
+    {imgUrl:"/img/re5.png",name:playlistDetail5.name,trackState:tracks6}
+  ]
+  // 从列表中随机选择三个元素，打乱，挑前五个
+  const shuffledList = [...list].sort(() => Math.random() - 0.5);//打乱
+  const selectedPlaylists = shuffledList.slice(0, 5);
   const navigate = useNavigate(); // 获取 useNavigate 函数
-  const handlePlaylistClick = () => {
-    // 在这里进行页面导航
-    navigate("/songlist", {
-      state: { tracks: tracks },
-    });
-  };
-  const handlePlaylistClick1 = () => {
-    // 在这里进行页面导航
-    navigate("/songlist", { state: { tracks: tracks2 } });
-  };
-  const handlePlaylistClick2 = () => {
-    // 在这里进行页面导航
-    navigate("/songlist", { state: { tracks: tracks3 } });
-  };
-  const handlePlaylistClick3 = () => {
-    // 在这里进行页面导航
-    navigate("/songlist", { state: { tracks: tracks4 } });
-  };
-  const handlePlaylistClick4 = () => {
-    // 在这里进行页面导航
-    navigate("/songlist", { state: { tracks: tracks5 } });
-  };
+  
 
   useEffect(() => {
     // 构建API请求的URL
@@ -95,6 +87,7 @@ const Homerecommend = () => {
     const apiUrl2 = "http://localhost:3000/playlist/detail?id=7437331292";
     const apiUrl3 = "http://localhost:3000/playlist/detail?id=7260028404";
     const apiUrl4 = "http://localhost:3000/playlist/detail?id=7822788988";
+    const apiUrl5 = "http://localhost:3000/playlist/detail?id=7068025847";
     // 发送GET请求
     // 使用Promise.all同时发送多个请求
     Promise.all([
@@ -103,16 +96,18 @@ const Homerecommend = () => {
       axios.get(apiUrl2),
       axios.get(apiUrl3),
       axios.get(apiUrl4),
+      axios.get(apiUrl5)
     ])
       .then((responses) => {
         // 处理API响应数据
-        const [response1, response2, response3, response4, response5] =
+        const [response1, response2, response3, response4, response5,response6] =
           responses;
         setPlaylistDetail(response1.data.playlist);
         setPlaylistDetail1(response2.data.playlist);
         setPlaylistDetail2(response3.data.playlist);
         setPlaylistDetail3(response4.data.playlist);
         setPlaylistDetail4(response5.data.playlist);
+        setPlaylistDetail5(response6.data.playlist);
 
         // 提取 tracks 值并存储在 tracks 变量中
         const extractedTracks = Object.values(response1.data.playlist.tracks);
@@ -125,6 +120,8 @@ const Homerecommend = () => {
         setTracks4(extractedTracks4);
         const extractedTracks5 = Object.values(response5.data.playlist.tracks);
         setTracks5(extractedTracks5);
+        const extractedTracks6 = Object.values(response6.data.playlist.tracks);
+        setTracks6(extractedTracks6);
       })
       .catch((error) => {
         console.error("API请求失败", error);
@@ -151,39 +148,13 @@ const Homerecommend = () => {
       </div>
       <Container style={{ display: "flex", marginLeft: "10px" }}>
     
-        <PlaylistItem
-          imageUrl="/img/re1.png"
-          playlistName={playlistDetail.name}
-          onClick={handlePlaylistClick}
-        />
-      
-    
+      {selectedPlaylists.map((item) => (
           <PlaylistItem
-            imageUrl="/img/re2.png"
-            playlistName={playlistDetail1.name}
-            onClick={handlePlaylistClick1}
+            imageUrl={item.imgUrl}
+            playlistName={item.name}
+            onClick={() => navigate("/songlist", { state: { tracks: item.trackState } })}
           />
-      
-        
-          <PlaylistItem
-            imageUrl="/img/re3.png"
-            playlistName={playlistDetail2.name}
-            onClick={handlePlaylistClick2}
-          />
-      
-     
-          <PlaylistItem
-            imageUrl="/img/re4.png"
-            playlistName={playlistDetail3.name}
-            onClick={handlePlaylistClick3}
-          />
-      
-       
-          <PlaylistItem
-            imageUrl="/img/re5.png"
-            playlistName={playlistDetail4.name}
-            onClick={handlePlaylistClick4}
-          />
+        ))}
        
       </Container>
     </>
